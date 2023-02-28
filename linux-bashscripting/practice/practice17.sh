3346c33a97f1f9b0809069ec0a85ebea17ea78e6
Gulraiz Gulshan <44838822+gulraizgulshan2k18@users.noreply.github.com>
2023-02-28 20:49:27 +0500

    update

---
+++ linux-bashscripting/practice/practice17.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#  Changes every filename in working directory to all lowercase.
+
+for filename in *                # Traverse all files in directory.
+do
+   fname=`basename $filename`
+   n=`echo $fname | tr A-Z a-z`  # Change name to lowercase.
+   if [ "$fname" != "$n" ]       # Rename only files not already lowercase.
+   then
+     mv $fname $n
+   fi  
+done   
+
+exit $?
